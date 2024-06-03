8ecdf1508595b33d200b8112e072abfea129b094
Bill Riehl <wjriehl@lbl.gov>
2024-06-03 10:53:04 -0400

    comment for conftest

--- test/conftest.py
+++ test/conftest.py
@@ -6,6 +6,7 @@ import pytest
 
 @pytest.fixture
 def config():
+    """Load and return the test config as a dictionary."""
     config_file = os.environ.get("KB_DEPLOYMENT_CONFIG", None)
     config = ConfigParser()
     config.read(config_file)
